d494c4c72e91af3e1599c749d19c91a0f20addf6
bilal-23 <mansuribilal101@gmail.com>
2021-06-12 16:40:40 +0530

    started code

---
+++ src/components/Board.js
@@ -0,0 +1,31 @@
+import Square from './Sqaure';
+
+const Board = () => {
+    const renderSquare = (i) => {
+        return <Square />;
+    }
+    const status = 'Next Player : X';
+
+    return (
+        <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
+};
+
+export default Board;
---
+++ src/components/Sqaure.js
@@ -0,0 +1,10 @@
+
+const Square = (props) => {
+    return (
+        <button className="square">
+            {props.value}
+        </button>
+    )
+}
+
+export default Square;
